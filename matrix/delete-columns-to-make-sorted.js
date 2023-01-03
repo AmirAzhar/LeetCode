3759f1eb3df1246e63d81ef622ce6b2c67147bb0
Azhar, Amir <amir97azhar@gmail.com>
2023-01-03 12:27:46 +0800

    #LC20230103

--- matrix/delete-columns-to-make-sorted.js
+++ matrix/delete-columns-to-make-sorted.js
@@ -7,15 +7,12 @@ var minDeletionSize = function (strs) {
 
   // Iterate through each character (col)
   for (let col = 0; col < strs[0].length; col++) {
-    // Iterate through each string (row)
-    for (let row = 0; row < strs.length; row++) {
-      // As long as it is not the last character of the 'column'
-      if (strs[row + 1]) {
-        // if the next char is lexicographically greater than the current, increase the count and move on to the next column
-        if (strs[row][col] > strs[row + 1][col]) {
-          count += 1;
-          break;
-        }
+    // Iterate through each string (row), but stop at the 2nd last row
+    for (let row = 0; row < strs.length - 1; row++) {
+      // if the next char is lexicographically greater than the current, increase the count and move on to the next column
+      if (strs[row][col] > strs[row + 1][col]) {
+        count += 1;
+        break;
       }
     }
   }
