da770c16253713105da6ecaaf552d37994f80fee
AmirAzhar <amir97azhar@gmail.com>
2023-04-01 11:59:38 +0800

    LC Blind 75 Two Sum

---
+++ arrays/two-sum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(target - nums[i]) != null)
+                return new int[] { i, map.get(target - nums[i]) };
+            map.put(nums[i], i);
+        }
+        return new int[] { -1, -1 };
+    }
+}
+
+// Better Solution
+// Init a map -> key (number) : value (index)
+// Iterate through the list
+// we check if (target - current) is in map
+// if in map, we have our 2 indices already
+// if not in map, add the number:index into the map
+// O(n)
