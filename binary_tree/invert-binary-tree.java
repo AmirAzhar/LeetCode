34e0bb98eabbdea3d2eef206f2e4e50418bf5fc8
AmirAzhar <amir97azhar@gmail.com>
2023-04-09 12:31:29 +0800

    LC Blind 75 - Invert Binary Tree

---
+++ binary_tree/invert-binary-tree.java
@@ -0,0 +1,21 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/description/
+
+package binary_tree;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null)
+            return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
