e6c8cc1922c923ef33c88136362e9f160522963e
AmirAzhar <amir97azhar@gmail.com>
2023-04-10 15:50:30 +0800

    LC Blind 75 - Subtree of Another Tree

---
+++ binary_tree/subtree-of-another-tree.java
@@ -0,0 +1,28 @@
+// 572. Subtree of Another Tree
+// https://leetcode.com/problems/subtree-of-another-tree/
+
+package binary_tree;
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null)
+            return false;
+
+        if (root.val == subRoot.val && isSameTree(root, subRoot))
+            return true;
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null || p.val != q.val)
+            return false;
+
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+    }
+}
