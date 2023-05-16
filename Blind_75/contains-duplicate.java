c8525ab63acee21fda7a231e79c549ad00c2ce6f
AmirAzhar <itooth55@gmail.com>
2023-05-16 16:38:28 +0800

    LC Blind 75 - Contains Duplicate

--- Blind_75/contains-duplicate.java
+++ Blind_75/contains-duplicate.java
@@ -1,6 +1,9 @@
 // 217. Contains Duplicate
 // https://leetcode.com/problems/contains-duplicate/
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<Integer>();
