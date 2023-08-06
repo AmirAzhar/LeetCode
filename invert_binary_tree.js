b90738cf3e1469ea1933f36ff555efbc5951f5c9
AmirAzhar <itooth55@gmail.com>
2023-08-06 10:39:22 +0800

    Grind 169 - invert_binary_tree

---
+++ invert_binary_tree.js
@@ -0,0 +1,32 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) return null;
+
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
+
+// do it recursively
+// i swap the child of the root first
+// then i move on to swap the children of above children that i already swapped
+// this continues until it reaches a null node
