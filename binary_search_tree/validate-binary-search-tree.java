a39a96c63cd649c288f9337a66e50268cc568c29
Amir Azhar <itooth55@gmail.com>
2025-05-24 17:44:02 +0800

    do 1 qn

--- binary_search_tree/validate-binary-search-tree.java
+++ binary_search_tree/validate-binary-search-tree.java
@@ -16,3 +16,17 @@ var isValidBST = function(root) {
 
     return validate(root, -Infinity, Infinity)
 };
+
+// Java
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean validate(TreeNode root, long min, long max) {
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+
+        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+    }
+}
